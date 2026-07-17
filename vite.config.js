ab100b9560c75104fe00f8bb2326cab2a6d3b025
lartfricain226 <lartfricainbf@gmail.com>
2026-07-17 01:45:44 +0000

    Build pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
